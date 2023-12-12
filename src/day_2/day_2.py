a6a6e24520cc43078166f91d2715f98a336dca10
rcarausu <rcarausu@mail.com>
2023-12-12 23:41:40 +0100

    add day 2 part 2

--- src/day_2/day_2.py
+++ src/day_2/day_2.py
@@ -1,28 +1,44 @@
 import re
 
 
-def _is_game_possible(game_record: list[str], bag: dict) -> bool:
+def _evaluate_game(game_record: list[str], bag: dict) -> tuple[bool, int]:
+    is_possible = True
+    minimum_bag = {
+        'red': 0,
+        'blue': 0,
+        'green': 0
+    }
+
     for record in game_record:
         record_results = record.split(',')
         for result in record_results:
             result_color = re.compile(r'[a-z]+').search(result).group()
             result_value = int(re.compile(r'[0-9]+').search(result).group())
-            if result_color not in bag.keys() \
-                    or result_value > bag[result_color] \
+
+            if result_value > minimum_bag[result_color]:
+                minimum_bag[result_color] = result_value
+
+            if result_value > bag[result_color] \
                     or result_value < 0:
-                return False
-    return True
+                is_possible = False
+
+    power = minimum_bag['red'] * minimum_bag['blue'] * minimum_bag['green']
+
+    return is_possible, power
 
 
-def find_possible_games(games_input: str, bag: dict) -> list[int]:
+def find_possible_games(games_input: str, bag: dict) -> tuple[list[int], list[int]]:
     possible_games = []
+    power_per_game = []
     games = games_input.strip().splitlines()
     for game in games:
         game_record = game[game.find(':') + 1:].strip().split(';')
-        if _is_game_possible(game_record, bag):
+        is_possible, power = _evaluate_game(game_record, bag)
+        if is_possible:
             game_id = re.compile(r'[0-9]+').search(game[:game.find(':')]).group()
             possible_games.append(int(game_id))
-    return possible_games
+        power_per_game.append(power)
+    return possible_games, power_per_game
 
 
 if __name__ == '__main__':
@@ -30,10 +46,10 @@ if __name__ == '__main__':
         bag = {
             'red': 12,
             'green': 13,
-            'blue': 14,
+            'blue': 14
         }
 
-        possible_games_ids = find_possible_games(file.read(), bag)
+        possible_games_ids, power_per_game = find_possible_games(file.read(), bag)
 
-        print(possible_games_ids)
         print(sum(possible_games_ids))
+        print(sum(power_per_game))
--- tests/test_day_2.py
+++ tests/test_day_2.py
@@ -4,19 +4,23 @@ from day_2.day_2 import find_possible_games
 
 
 class TestDay2(unittest.TestCase):
+    text = """
+    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    """
+    bag = {
+        'red': 12,
+        'blue': 13,
+        'green': 14
+    }
 
     def test_part_1(self):
-        text = """
-        Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-        Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-        Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-        Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-        Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-        """
-        bag = {
-            'red': 12,
-            'blue': 13,
-            'green': 14,
-        }
+        possible_games, ignore = find_possible_games(self.text, self.bag)
+        self.assertEqual(sum(possible_games), 8)
 
-        self.assertEqual(sum(find_possible_games(text, bag)), 8)
+    def test_part_2(self):
+        ignore, power_per_game = find_possible_games(self.text, self.bag)
+        self.assertEqual(sum(power_per_game), 2286)
